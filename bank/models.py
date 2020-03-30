4b5671d3cf396227f74992645fc723ad639a3401
root <root@django.southamerica-east1-b.c.pelagic-river-264103.internal>
2020-03-30 00:07:52 +0000

    modified:   bank/models.py

--- bank/models.py
+++ bank/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Account(models.Model):
+    accountnumber = models.BigIntergerField(primaryKey=True, editable=True)
+    name = models.CharField(max_length=30)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
